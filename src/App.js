7dce94c4bb10b6c0ac63ef663ee7cf90e30861b7
fboussad <faouziboussad@gmail.com>
2022-08-26 10:13:36 +0200

    dark and light mode updates
    remove the function connected to the DOM use the class dark on APP object instead of on html tag

--- src/App.js
+++ src/App.js
@@ -6,29 +6,19 @@ import DarkImage from "./img/moon.png"
 
 function App() {
     const [coinName, setCoinName] = useState("Bitcoin")
-    const [toggleMode, setToggleMode] = useState("dark")
-
-    const handleToggleMode = () => {
-        if (toggleMode === "dark") {
-            document.getElementsByTagName("html")[0].className = "light"
-            setToggleMode("light")
-        } else {
-            document.getElementsByTagName("html")[0].className = "dark"
-            setToggleMode("dark")
-        }
-    }
+    const [isDark, setIsDark] = useState(true)
 
     return (
-        <>
+        <div className={isDark ? "dark":"light"}>
             <header className="flex justify-between px-8 py-4 text-xl font-semibold border-b dark:border-gray-600 shadow-sm dark:bg-gray-800 dark:text-white">
                 <h1>
                     C<span className="text-red-600">Dashboard</span>
                 </h1>
                 <div
-                    onClick={handleToggleMode}
+                    onClick={()=> setIsDark(!isDark)}
                     className="cursor-pointer text-sm mr-12"
                 >
-                    {toggleMode === "dark" ? (
+                    {isDark ? (
                         <img src={LightImage} className="h-8" alt="Light" />
                     ) : (
                         <img src={DarkImage} className="h-8" alt="Dark" />
@@ -39,7 +29,7 @@ function App() {
                 <Home setCoinName={setCoinName} />
                 <Dashbord coinName={coinName.toLowerCase()} />
             </main>
-        </>
+        </div>
     )
 }
 
