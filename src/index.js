f062b624e789dff557a92ae771bf3570ed6faefa
fboussad <faouziboussad@gmail.com>
2022-09-20 13:42:05 +0200

    remove react-query dev tool

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { QueryClient, QueryClientProvider } from "react-query"
-import { ReactQueryDevtools } from "react-query/devtools"
 
 import "./index.css"
 import App from "./App"
@@ -18,7 +17,6 @@ root.render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
             <App />
-            <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
     </React.StrictMode>
 )
